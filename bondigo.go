5b9e75d39e917b4f4ac3195fcb65dfa03b9ab81a
ctrl4 <mrmateofleitas+git@gmail.com>
2021-01-02 00:05:32 -0300

    Finally I'm able to traverse an unestructured json

--- bondigo.go
+++ bondigo.go
@@ -3,30 +3,37 @@ import (
 	"fmt"
 	"net/http"
 	"flag"
+	"io/ioutil"
 	"encoding/json"
+//	"reflect"
 )
 
 func main(){
-	var parada string 
-	var bondi string
-	flag.StringVar(&parada, "parada", "2664", "Numero de parada a consultar.")
-	flag.StringVar(&bondi, "bondi", "0", "Numero de bondi para filtrar.")
+	parada := flag.String("parada", "2664", "Numero de parada a consultar.")
+//	bondi := flag.String("bondi", "0", "Numero de bondi para filtrar.")
 	flag.Parse()
 
 
-	resp, err := http.Get("http://api.montevideo.gub.uy/transporteRest/siguientesParada/" + parada)
+	resp, err := http.Get("http://api.montevideo.gub.uy/transporteRest/siguientesParada/" +  *parada)
 	if err != nil {
         	print(err)
     	}
 	defer resp.Body.Close()
-	var data map[string]interface{}
-	json.NewDecoder(resp.Body).Decode(&data)
 
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
         	print(err)
     	}
+	sb := string(body)
+  var results []map[string]interface{}
+	json.Unmarshal([]byte(sb),&results)
 
-	for _, lineas := range data{
-		fmt.Println(lineas)
+	for key, result := range results {
+		fmt.Println("Reading Value for Key :", key)
+		fmt.Println(
+			"- Linea: ",result["linea"],
+			"- Minutos: ", result["minutos"],
+			"- Real: ", result["real"])
 	}
-}
+
+		}
